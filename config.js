234ac9b5e15704dbf32001e4750da3cdab2008e5
rikyanu <rikyanu86@gmail.com>
2026-03-17 21:48:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+window.RATFINK_CONFIG = {
+    telegram: {
+        BOT_TOKEN: '8711546914:AAFPV_Cs2ALUd6b1rJ56LM9fQy3z9axqoqs', // GANTI!
+        GROUP_ID: '-1003858577739', // GANTI!
+        NOTIFICATION_CHAT: '-1002233445566' // GANTI!
+    },
+    access: {
+        ADMINS: [7742916370], // GANTI!
+        MEMBERS: []
+    }
+};
